84a15c7a1cd30bd2b7b1af5809da66b470002fc7
David Waddington <davidw93.dw@gmail.com>
2012-10-26 12:59:48 -0700

    update before format

--- src/loader.s
+++ src/loader.s
@@ -3,10 +3,10 @@ _start:
   /* First we set the stack pointer to 0x8000 as this is the value
    * Set by broadcom for where the kernel actually starts
   */
-  mov sp, #0x8000
+  mov sp,#0x8000
   /* Branch and link (copy the address of the next instruction to LR) */
   /* LR == Link Register (R14) */
- bl start_kernel
+  bl start_kernel
 
 /* Once we've finished loading, throw ourselves into an infinite loop */
 /* This essentially allows us to stop the processor in a nice way to remove power */
@@ -15,7 +15,7 @@ hang: b hang
 .globl P32
 P32:
   /* Store value of r0 to register r1 */
-  str r1, [r0]
+  str r1,[r0]
   /* Returns from the function */
   bx lr
 
@@ -23,7 +23,7 @@ P32:
 G32:
   /* We obviously don't want to change the register by getting it */
   /* So load r0 back into r0 */
-  ldr r0, r0
+  ldr r0,[r0]
   /* Returns from the function */
   bx lr
 
