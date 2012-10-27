7deedd8fff4b6b70b6bcc041133a768e261e8557
David Waddington <davidw93.dw@gmail.com>
2012-10-27 05:46:02 -0700

    updated structure

--- src/led.c
+++ src/led.c
@@ -2,11 +2,13 @@ extern void P32 ( unsigned int, unsigned int );
 extern unsigned int G32 ( unsigned int );
 extern void freeze ( unsigned int );
 
+#define SYSTIMERCLO 0x20003004
 #define GPFSEL1 0x20200004
 #define GPSET  0x2020001C
 #define GPCLR  0x20200028
+#define TIMERBIT 0x00400000
 
-int start_kernel( void ) {
+void blink_active( void ) {
     unsigned int rand_var;
 
     rand_var = G32(GPFSEL1);
@@ -16,9 +18,14 @@ int start_kernel( void ) {
 
     while( 1 ) {
         P32(GPSET, 1<<16);  /* We want to set it to 16 due to that being the LED for active */
-        for(rand_var=0;rand_var<0x100000;rand_var++) freeze(rand_var);
+        while( 1 ) {
+            rand_var=G32(SYSTIMERCLO);
+            if((rand_var&=TIMERBIT)==TIMERBIT) break;
+        }
         P32(GPCLR, 1<<16);
-        for(rand_var=0;rand_var<0x100000;rand_var++) freeze(rand_var);
+        while( 1 ) {
+            rand_var=G32(SYSTIMERCLO);
+            if((rand_var&=TIMERBIT)==0) break;
+        }
     }
-    return 0;
 }
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "led.h"
+
+int start_kernel( void ){
+    blink_active();
+    return 0;
+}
